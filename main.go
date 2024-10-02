d02ef583332e5a424c703275e16a55ec279f56f4
Andrew Snodgrass <pointlander@gmail.com>
2024-10-02 18:06:05 -0500

    Load iris data

--- main.go
+++ main.go
@@ -5,8 +5,13 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
 	"embed"
+	"encoding/csv"
+	"fmt"
 	"io"
+	"strconv"
 )
 
 //go:embed iris.zip
@@ -24,4 +29,44 @@ func main() {
 		panic(err)
 	}
 	_ = data
+
+	type Fisher struct {
+		Measures [4]float64
+		Label    string
+	}
+	fisher := make([]Fisher, 0, 8)
+	reader, err := zip.NewReader(bytes.NewReader(data), int64(len(data)))
+	if err != nil {
+		panic(err)
+	}
+	for _, f := range reader.File {
+		if f.Name == "iris.data" {
+			iris, err := f.Open()
+			if err != nil {
+				panic(err)
+			}
+			reader := csv.NewReader(iris)
+			data, err := reader.ReadAll()
+			if err != nil {
+				panic(err)
+			}
+			for _, item := range data {
+				record := Fisher{
+					Label: item[4],
+				}
+				for i := range item[:4] {
+					f, err := strconv.ParseFloat(item[i], 64)
+					if err != nil {
+						panic(err)
+					}
+					record.Measures[i] = f
+				}
+				fisher = append(fisher, record)
+			}
+			iris.Close()
+		}
+	}
+	for _, item := range fisher {
+		fmt.Println(item)
+	}
 }
